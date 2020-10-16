4969c2ce678521d45231b5f25fa80c1957b0aebd
Lyatuu <daniellyatuu@gmail.com>
2020-10-16 14:34:08 +0300

    updates

--- app/main/views.py
+++ app/main/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views import View
 
-# Create your views here.
+class HomeView(View):
+    template_name = 'main/home.html'
+
+    def get(self, request, *args, **kwargs):
+        context = {}
+        context['title']='Huru'
+        return render(request, self.template_name, context)
