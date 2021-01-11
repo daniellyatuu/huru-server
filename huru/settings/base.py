15d3e222b2962a16edd377e66c301c8aa264d2a6
Lyatuu <daniellyatuu@gmail.com>
2021-01-11 14:14:00 +0300

    update

--- huru/settings/base.py
+++ huru/settings/base.py
@@ -45,8 +45,8 @@ AUTH_USER_MODEL = 'user.User'
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
     'django.middleware.locale.LocaleMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
